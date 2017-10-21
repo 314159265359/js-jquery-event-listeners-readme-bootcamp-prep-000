34ab86363a501ae5c6fb32f694d9110f1ccb1a45
Karen G <kgraves0701@gmail.com>
2017-10-21 03:18:00 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -13,11 +13,11 @@ function frameIt() {
 function submitIt() {
   $("form").on("submit", function() {
   alert('Your form is going to be submitted now.');
-  }
+});
 }
 
 $(document).ready(function(){
   getIt()
   frameIt()
   submitIt()
-}
+});
