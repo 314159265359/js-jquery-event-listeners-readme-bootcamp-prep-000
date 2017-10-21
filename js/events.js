3e33abfdea69a7c89618b9e60c7e61069257586b
Karen G <kgraves0701@gmail.com>
2017-10-21 03:26:57 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -17,7 +17,11 @@ function submitIt() {
 }
 
 function pressIt(){
-
+  $(document).on('keydown', function(key) {
+  if(key.which == 71){
+      alert('g was pressed');
+  }
+});
 }
 
 $(document).ready(function(){
