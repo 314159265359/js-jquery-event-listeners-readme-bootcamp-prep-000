cb30ac202342ec5a91187e9548eb176c8cb96f19
Karen G <kgraves0701@gmail.com>
2017-10-21 03:20:39 +0000

    automatically backed up by learn

--- js/events.js
+++ js/events.js
@@ -1,26 +1,26 @@
 function getIt() {
   $('p').on('click', function(){
   alert("hey!")
- });
+  });
 }
 
 function frameIt() {
   $('img').on('load', function(){
    $('img').addClass('tasty');
-});
+  });
 }
 
 function submitIt() {
   $("form").on("submit", function() {
   alert('Your form is going to be submitted now.');
-});
+  });
 }
 
 function pressIt(){
   $(document).on('keydown', function(key) {
   if(key.which == 71){
       alert('g was pressed');
-});
+  });
 }
 
 $(document).ready(function(){
